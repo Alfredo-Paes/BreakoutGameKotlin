d9105f28dc8614bccb8e08bb4a651613525b3230
Alfredo Paes da Luz <alfredo.alfpaes@gmail.com>
2024-10-28 00:13:26 -0300

    Implementação para o direcionamento do aperta do botão para a tela de GameView

---
+++ app/src/main/java/com/alfredopaesdaluz/breakoutgamekotlin/GameView.kt
@@ -0,0 +1,6 @@
+package com.alfredopaesdaluz.breakoutgamekotlin
+
+import android.content.Context
+import android.view.View
+
+class GameView(context: Context) : View(context)
--- app/src/main/java/com/alfredopaesdaluz/breakoutgamekotlin/MainActivity.kt
+++ app/src/main/java/com/alfredopaesdaluz/breakoutgamekotlin/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.alfredopaesdaluz.breakoutgamekotlin
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.ImageButton
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var text2: TextView
+    private lateinit var startbtn: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainActivity)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        text2 = findViewById(R.id.text2)
+        startbtn = findViewById(R.id.startbtn)
+    }
+
+    fun startGame(view: View) {
+        val gameView = GameView(this)
+        setContentView(gameView)
     }
 }
